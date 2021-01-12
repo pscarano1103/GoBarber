2666d50f8c5fb0bc5a9b07bd090488fa38d3b547
Paulo_Scarano <scaranopaulo@gmail.com>
2021-01-11 22:23:11 -0300

    criando pagina de login

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignIn: React.FC = () => <Container />;
+import { Container, Title } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Image source={logoImg} />
+    <Title>Faça seu login</Title>
+
+    <Input name="email" icon="mail" placeholder="E-mail" />
+    <Input name="password" icon="lock" placeholder="Senha" />
+
+    <Button
+      onPress={() => {
+        console.log('Deu');
+      }}
+    >
+      Entrar
+    </Button>
+  </Container>
+);
 
 export default SignIn;

a6b60b0a6a37975399b21e992e799a0d508f2b9a
Paulo_Scarano <scaranopaulo@gmail.com>
2021-01-18 22:14:38 -0300

    application registration

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -16,10 +16,9 @@ import { FormHandles } from '@unform/core';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
 
-import {
- Container, Title, BackToSignIn, BackToSignInText
-} from './styles';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -52,9 +51,13 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
 
-      // history.push('/');
+      Alert.alert(
+        'Cadastro realizado com sucesso',
+        'Você ja pode fazer lgoin na aplicação',
+      );
+      navigation.goBack();
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);

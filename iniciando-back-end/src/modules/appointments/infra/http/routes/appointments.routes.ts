a472de7b4dc972c96ef95f48138964eb07187476
Paulo_Scarano <scaranopaulo@gmail.com>
2021-01-27 21:55:19 -0300

    aplicando conceitos de SOLID

--- iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,7 +2,6 @@
 import 'reflect-metadata';
 
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -10,23 +9,25 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  console.log(request.user);
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+//  console.log(request.user);
+//  const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//  return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,

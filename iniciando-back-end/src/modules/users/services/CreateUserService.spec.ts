65e342c512cd318245d3425ad3a9b1f5a40f01ff
Paulo_Scarano <scaranopaulo@gmail.com>
2021-02-09 06:42:31 -0300

    implementando testes parte 2

--- iniciando-back-end/src/modules/users/services/CreateUserService.spec.ts
+++ iniciando-back-end/src/modules/users/services/CreateUserService.spec.ts
@@ -2,50 +2,47 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
-
 import CreateUserService from './CreateUserService';
-
 
 describe('CreateUser', () => {
   it('should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository;
-    const fakeHashProvider = new FakeHashProvider;
-    const CreateUser = new CreateUserService(
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
       fakeUsersRepository,
-      fakeHashProvider
+      fakeHashProvider,
     );
 
-    const  user = await CreateUser.execute({
+    const user = await createUser.execute({
       name: 'John Doe',
-      email: 'john.doe@john.com.br',
-      password: '123456'
+      email: 'johndoe@example.com',
+      password: '123123',
     });
 
     expect(user).toHaveProperty('id');
   });
 
-  it('should not be able to create a new user with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository;
-    const fakeHashProvider = new FakeHashProvider;
-    const CreateUser = new CreateUserService(
+  it('should not be able to create a new user with email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
       fakeUsersRepository,
-      fakeHashProvider
+      fakeHashProvider,
     );
 
-    await CreateUser.execute({
+    await createUser.execute({
       name: 'John Doe',
-      email: 'john.doe@john.com.br',
-      password: '123456'
+      email: 'johndoe@example.com',
+      password: '123123',
     });
 
     expect(
-      CreateUser.execute({
+      createUser.execute({
         name: 'John Doe',
-        email: 'john.doe@john.com.br',
-        password: '123456'
+        email: 'johndoe@example.com',
+        password: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
-
-
-})
+});

e83ba683571301d25bd8c72d5328f0f066faa7b4
Paulo_Scarano <scaranopaulo@gmail.com>
2021-02-20 13:25:09 -0300

    Agendamentos

--- iniciando-back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ iniciando-back-end/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,9 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthfromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayfromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllInMonthfromProvider(data: IFindAllInMonthfromProviderDTO): Promise<Appointment[]>;
+  findAllInDayFromProvider(data: IFindAllInDayfromProviderDTO): Promise<Appointment[]>;
 }

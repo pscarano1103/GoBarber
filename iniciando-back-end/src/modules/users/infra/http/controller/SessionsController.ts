6554db57fa96be29d496ee53e3e3bc375c37f064
Paulo_Scarano <scaranopaulo@gmail.com>
2021-03-01 06:44:43 -0300

    usingClassTransformer

--- iniciando-back-end/src/modules/users/infra/http/controller/SessionsController.ts
+++ iniciando-back-end/src/modules/users/infra/http/controller/SessionsController.ts
@@ -1,6 +1,7 @@
 
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
@@ -15,14 +16,6 @@ export default class SessionsController {
       password,
     });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json({ user: userWithoutPassword, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
--- iniciando-back-end/src/modules/users/infra/http/controller/UserAvatarController.ts
+++ iniciando-back-end/src/modules/users/infra/http/controller/UserAvatarController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
-
+import { classToClass } from 'class-transformer';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
 export default class UserAvatarController {
@@ -12,14 +12,6 @@ export default class UserAvatarController {
       avatarFilename: request.file.filename,
     });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 }

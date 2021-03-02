a30054ca195695455beeaf2042985456c4cbdc1f
Paulo_Scarano <scaranopaulo@gmail.com>
2021-03-02 06:45:54 -0300

    configuringAWS_SES

---
+++ iniciando-back-end/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    }
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'paulo@techdev.com',
+      name: 'Paulo da TechDev',
+    },
+  },
+} as IMailConfig;
